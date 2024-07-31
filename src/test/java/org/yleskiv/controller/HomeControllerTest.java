53dce6a260d888b9c9e97ac4fe2c92b4a5710b77
Yurii <leskiv.yurii@gmail.com>
2024-07-31 18:23:45 +0300

    BK-11 [Yurii Leskiv] added unit tests for home controller

---
+++ src/test/java/org/yleskiv/controller/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package org.yleskiv.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void home() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("/index"));
+    }
+
+}
